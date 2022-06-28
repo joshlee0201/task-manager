4c478bc1dc3e423a5084c3c41b3ee7b038935627
Josh <joshlee0201@gmail.com>
2022-06-28 10:59:09 -0700

    heroku commit

--- tracker/settings.py
+++ tracker/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import dj_database_url
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,8 +84,13 @@ WSGI_APPLICATION = "tracker.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-import dj_database_url
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(prod_db)
 
